112059e46cc606366bb74543ddd9d53b1d9ddeb6
Juniper <juneconsolidated@gmail.com>
2021-11-09 12:29:57 -0800

    Removed Uncalled Variable

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -2,7 +2,6 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-import { useLocation } from "react-router";
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
